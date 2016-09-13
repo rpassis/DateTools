fe8f86f87ba04e79e131dc7ad82acbe0c460796d
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-13 14:13:04 +1000

    Framework

--- DateTools/DateTools.h
+++ DateTools/DateTools.h
@@ -1,29 +1,26 @@
-// Copyright (C) 2014 by Matthew York
 //
-// Permission is hereby granted, free of charge, to any
-// person obtaining a copy of this software and
-// associated documentation files (the "Software"), to
-// deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of the
-// Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  DateTools.h
+//  DateTools
 //
-// The above copyright notice and this permission notice shall
-// be included in all copies or substantial portions of the Software.
+//  Created by Rogerio de Paula Assis on 13/09/2016.
+//  Copyright © 2016 Freshmob Pty Ltd. All rights reserved.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import "DTConstants.h"
-#import "DTError.h"
-#import "NSDate+DateTools.h"
-#import "DTTimePeriod.h"
-#import "DTTimePeriodGroup.h"
-#import "DTTimePeriodCollection.h"
-#import "DTTimePeriodChain.h"
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateTools.
+FOUNDATION_EXPORT double DateToolsVersionNumber;
+
+//! Project version string for DateTools.
+FOUNDATION_EXPORT const unsigned char DateToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateTools/PublicHeader.h>
+
+
+#import <DateTools/DTConstants.h>
+#import <DateTools/DTError.h>
+#import <DateTools/NSDate+DateTools.h>
+#import <DateTools/DTTimePeriod.h>
+#import <DateTools/DTTimePeriodGroup.h>
+#import <DateTools/DTTimePeriodCollection.h>
+#import <DateTools/DTTimePeriodChain.h>
